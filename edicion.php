f1dc384c109631c7f551a268e5fe35d041164bd6
AlejMtz <145378489+AlejMtz@users.noreply.github.com>
2024-02-21 15:49:05 -0600

    crud modificar funcional

--- edicion.php
+++ edicion.php
@@ -36,15 +36,17 @@
     <input type="number" id="Nstock" name="Nstock" step="1" value="<?php echo $stock;?>" required><br>
 
     <label for="Nimagen">Nueva Imagen:</label>
-    <input type="file" id="Nimagen" name="Nimagen" accept="image/*" onchange="previewImage()" value="<?php echo $imagen;?>"><br>
-    <img id="imagenPreview" alt="Imagen previa" style="width: 200px; display: none;" src="<?php echo $imagen;?>"><br>
+    <input type="file" id="Nimagen" name="Nimagen" accept="image/*"><br>
+    <img id="imagenPreview" alt="Imagen previa" style="width: 200px; display: none;"><br>
 
     <label for="Nmodelo3d">Nuevo Modelo 3D (.glb):</label>
-    <input type="file" id="Nmodelo3d" name="Nmodelo3d" accept=".glb" onchange="previewModel()" value="<?php echo $modelo3d;?>"><br>
+    <input type="file" id="Nmodelo3d" name="Nmodelo3d" accept=".glb"><br>
     <model-viewer id="modelo3dPreview" style="width: 200px; height: 200px;"></model-viewer><br>
 
     <button type="submit" name="act">Actualizar Producto</button>
 </form>
+
+
 
 <?php
 
@@ -54,8 +56,23 @@ if(isset($_POST['act'])){
     $actualizar_descripcion = $_POST['Ndescripcion'];
     $actualizar_precio = $_POST['Nprecio'];
     $actualizar_stock = $_POST['Nstock'];
-    $actualizar_imagen = $_POST['Nimagen'];
-    $actualizar_modelo3d = $_POST['Nmodelo3d'];
+
+    // Procesar nuevas imágenes y modelos 3D solo si se seleccionan archivos nuevos
+    if ($_FILES["Nimagen"]["size"] > 0) {
+        $imagen_path = "img/" . basename($_FILES["Nimagen"]["name"]);
+        move_uploaded_file($_FILES["Nimagen"]["tmp_name"], $imagen_path);
+        $actualizar_imagen = $imagen_path;
+    } else {
+        $actualizar_imagen = $imagen;
+    }
+
+    if ($_FILES["Nmodelo3d"]["size"] > 0) {
+        $modelo3d_path = "modelos3d/" . basename($_FILES["Nmodelo3d"]["name"]);
+        move_uploaded_file($_FILES["Nmodelo3d"]["tmp_name"], $modelo3d_path);
+        $actualizar_modelo3d = $modelo3d_path;
+    } else {
+        $actualizar_modelo3d = $modelo3d;
+    }
 
     $actualizar = "UPDATE productos SET nombre='$actualizar_nombre', descripcion='$actualizar_descripcion', precio='$actualizar_precio', stock='$actualizar_stock', imagen='$actualizar_imagen', modelo3d='$actualizar_modelo3d' WHERE id='$editar_id'";
 
@@ -67,5 +84,8 @@ if(isset($_POST['act'])){
     }
 }
 ?>
+
+
+
 
 </center>
